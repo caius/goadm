64d2e789c3b5ebb7c8df8c04d9104bd05e8c6be3
Caius Durling <dev@caius.name>
2019-03-06 02:23:47 +0100

    Wrapper ruby script
    Quicker to abstract the running of stuff, and the ssh-ness of things

---
+++ exe/wrapper.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby
+#
+#  wrapper.rb
+#  goadm
+#
+#  Basic wrapper intended for vmadm/imgadm given a node,
+#  and some commands to run
+#
+#  Created by Caius Durling on 2019-03-06.
+#  Copyright 2019 SwedishCampground. All rights reserved.
+#
+
+require "net/ssh"
+require "optparse"
+require "shellwords"
+
+host, user, port = ARGV.first(3)
+user ||= "root"
+port ||= 22
+port = Integer(port)
+
+remote_command = $stdin.read
+
+Net::SSH.start(host, user, port: port, non_interactive: true) do |ssh|
+  ssh.exec!(remote_command) do |channel, stream,
+    data|
+    case stream
+    when :stdout
+      $stdout.puts data
+    when :stderr
+      $stderr.puts data
+    end
+  end
+end
