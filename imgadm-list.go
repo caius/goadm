7e31f59ec1f85b5b9ef0e93ecc1c297e76953a61
Caius Durling <dev@caius.name>
2019-03-12 09:57:09 +0000

    imgadm-list can grab JSON to parse now

--- imgadm-list.go
+++ imgadm-list.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
 	"log"
+	"os/exec"
 )
 
 type Goadm struct {
@@ -15,23 +18,51 @@ func (g Goadm) Imgadm() (Imgadm, error) {
 	return imgadm, nil
 }
 
+func (g Goadm) Exec(command string) (string, error) {
+	cmd := exec.Command("./exe/wrapper.sh", g.Host, g.User, fmt.Sprintf("%d", g.Port), "--", command)
+
+	var out bytes.Buffer
+	cmd.Stdout = &out
+
+	var outerr bytes.Buffer
+	cmd.Stderr = &outerr
+
+	err := cmd.Run()
+	if err != nil {
+		fmt.Printf("err out: %q\n", outerr.String())
+		log.Fatal(err)
+	}
+
+	fmt.Printf("cmd out: %q\n", out.String())
+
+	return "", nil
+}
+
 type Imgadm struct {
 	goadm Goadm
 }
 
 type Image struct {
-	uuid      string
-	name      string
-	installed bool
+	Uuid      string
+	Name      string
+	Installed bool
 }
 
-func (i Imgadm) ListImages() ([Image], error) {
-	result, err := goadm.Exec("imgadm list -j")
-  if err != nil {
-    return nil, err
-  }
+func (i Imgadm) ListImages() ([]Image, error) {
+	result, err := i.goadm.Exec("imgadm list -j")
+	if err != nil {
+		return nil, err
+	}
 
-  return JSON.parse(result), nil
+	log.Printf("%s\n", result)
+
+	// return JSON.parse(result), nil
+	var images [1]Image
+	images[0] = Image{
+		Uuid: "97a044cd-10c3-4ba7-9fe4-933481e3474d",
+		Name: "debian9",
+	}
+	return images[:], nil
 }
 
 func main() {
@@ -46,8 +77,11 @@ func main() {
 		log.Fatal(err)
 	}
 
-	for image := range imgadm.ListImages() {
-		log.Info(image.Name)
+	images, err := imgadm.ListImages()
+	if err != nil {
+		log.Fatal(err)
 	}
-
+	for _, image := range images {
+		log.Printf("%s\t%s\n", image.Uuid, image.Name)
+	}
 }
