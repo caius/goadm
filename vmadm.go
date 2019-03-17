c073899a942b3f18e9af4bc973f43867f3011b69
Caius Durling <dev@caius.name>
2019-03-17 17:43:36 +0000

    Implement zone fetching

--- vmadm.go
+++ vmadm.go
@@ -2,6 +2,7 @@ package goadm
 
 import (
 	"encoding/json"
+	"fmt"
 )
 
 type Vmadm struct {
@@ -9,10 +10,23 @@ type Vmadm struct {
 }
 
 type ZonesJSON []struct {
-	Manifest struct {
-		Uuid string `json:"uuid"`
-		Name string `json:"name"`
-	} `json:"manifest"`
+	ZoneJSON
+}
+
+type ZoneJSON struct {
+	Autoboot          bool   `json:"autoboot"`
+	Brand             string `json:"brand"`
+	CpuShares         int    `json:"cpu_shares"`
+	DnsDomain         string `json:"dns_domain"`
+	ImageUuid         string `json:"image_uuid"`
+	MaxLockedMemory   int    `json:"max_locked_memory"`
+	MaxLwps           int    `json:"max_lwps"`
+	MaxPhysicalMemory int    `json:"max_physical_memory"`
+	MaxSwap           int    `json:"max_swap"`
+	Name              string `json:"name"`
+	State             string `json:"zone_state"`
+	Uuid              string `json:"uuid"`
+	ZfsIoPriority     int    `json:"zfs_io_priority"`
 }
 
 type Zone struct {
@@ -35,10 +49,25 @@ func (i Vmadm) ListZones() ([]Zone, error) {
 	var zones []Zone
 	for _, parsedZone := range parsedZones {
 		zones = append(zones, Zone{
-			Uuid: parsedZone.Manifest.Uuid,
-			Name: parsedZone.Manifest.Name,
+			Uuid: parsedZone.Uuid,
+			Name: parsedZone.Name,
 		})
 	}
 
 	return zones, nil
+}
+
+func (i Vmadm) GetZone(uuid string) (*Zone, error) {
+	result, err := i.exec(fmt.Sprintf("vmadm get %s", uuid))
+	if err != nil {
+		return nil, err
+	}
+
+	var parsedZone ZoneJSON
+	err = json.Unmarshal([]byte(result), &parsedZone)
+
+	return &Zone{
+		Uuid: parsedZone.Uuid,
+		Name: parsedZone.Name,
+	}, nil
 }
