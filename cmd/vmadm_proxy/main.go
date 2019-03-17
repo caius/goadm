6f16f4f9ac910e052285b9e4d60a4e8dca2bee2f
Caius Durling <dev@caius.name>
2019-03-17 17:41:27 +0000

    Tidy up & extend vmadm proxy a little
    * Its it's own module
    * Implement list & get subcommands

--- cmd/vmadm_proxy/main.go
+++ cmd/vmadm_proxy/main.go
@@ -3,22 +3,75 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/caius/goadm"
+	"gopkg.in/urfave/cli.v1"
 )
 
 func main() {
-	client := goadm.NewClient("127.0.0.1", "root", 2022)
 
-	vmadm := client.Vmadm()
-	zones, err := vmadm.ListZones()
-	if err != nil {
-		log.Fatal(err)
+	app := cli.NewApp()
+	app.Name = "vmadm_proxy"
+	app.Usage = "Proxy for vmadm on another host"
+
+	app.Commands = []cli.Command{
+		{
+			Name:    "list",
+			Aliases: []string{"ls"},
+			Usage:   "List zones",
+			Action: func(c *cli.Context) error {
+				client := goadm.NewClient("127.0.0.1", "root", 2022)
+
+				vmadm := client.Vmadm()
+				zones, err := vmadm.ListZones()
+				if err != nil {
+					return err
+				}
+
+				fmt.Printf("%s\t%s\n", "UUID", "Name")
+
+				for _, zone := range zones {
+					fmt.Printf("%s\t%s\n", zone.Uuid, zone.Name)
+				}
+
+				return nil
+			},
+		},
+		{
+			Name:  "get",
+			Usage: "Get information on a single zone",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Usage: "SmartOS Host to run against",
+				},
+				cli.StringFlag{
+					Name:  "user",
+					Usage: "User to login as",
+				},
+				cli.IntFlag{
+					Name:  "port",
+					Usage: "SSH Port on host",
+				},
+			},
+			Action: func(c *cli.Context) error {
+				client := goadm.NewClient(c.String("host"), c.String("user"), c.Int("port"))
+
+				vmadm := client.Vmadm()
+				zone, err := vmadm.GetZone(c.Args().Get(0))
+				if err != nil {
+					return err
+				}
+
+				fmt.Printf("%s\n", zone.Uuid)
+				return nil
+			},
+		},
 	}
 
-	fmt.Printf("Found %d zones\n", len(zones))
-
-	for _, zone := range zones {
-		fmt.Printf("%s\t%s\n", zone.Uuid, zone.Name)
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
 	}
 }
